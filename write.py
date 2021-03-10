fd23b817dba934d00251f120ea15f50c080dbb7f
Jannis Jehmlich <jjehmlich@yahoo.de>
2021-03-10 14:51:41 +0100

    inital commit

---
+++ write.py
@@ -0,0 +1,63 @@
+"""Write a stream of close approaches to CSV or to JSON.
+
+This module exports two functions: `write_to_csv` and `write_to_json`, each of
+which accept an `results` stream of close approaches and a path to which to
+write the data.
+
+These functions are invoked by the main module with the output of the `limit`
+function and the filename supplied by the user at the command line. The file's
+extension determines which of these functions is used.
+"""
+import csv
+import json
+import helpers
+
+
+def write_to_csv(results, filename):
+    """Write an iterable of `CloseApproach` objects to a CSV file.
+
+    The precise output specification is in `README.md`. Roughly, each output row
+    corresponds to the information in a single close approach from the `results`
+    stream and its associated near-Earth object.
+
+    :param results: An iterable of `CloseApproach` objects.
+    :param filename: A Path-like object pointing to where the data should be saved.
+    """
+    fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name', 'diameter_km', 'potentially_hazardous')
+    with open(filename, "w") as outfile:
+        writer = csv.writer(outfile)
+        writer.writerow(fieldnames)
+        for row in results:
+            writer.writerow([row.time, row.distance, row.velocity, row.neo.designation, row.neo.name, row.neo.diameter, row.neo.hazardous])
+
+
+def write_to_json(results, filename):
+    """Write an iterable of `CloseApproach` objects to a JSON file.
+
+    The precise output specification is in `README.md`. Roughly, the output is a
+    list containing dictionaries, each mapping `CloseApproach` attributes to
+    their values and the 'neo' key mapping to a dictionary of the associated
+    NEO's attributes.
+
+    :param results: An iterable of `CloseApproach` objects.
+    :param filename: A Path-like object pointing to where the data should be saved.
+    """
+    # list saving all the results in dictionary format
+    result_dict = []
+    for row in results:
+        entry = {
+            "datetime_utc": helpers.datetime_to_str(row.time),
+            "distance_au": row.distance,
+            "velocity_km_s": row.velocity,
+            "neo": {
+                "designation": row.neo.designation,
+                "name": row.neo.name,
+                "diameter_km": row.neo.diameter,
+                "potentially_hazardous": row.neo.hazardous
+            }
+        }
+        result_dict.append(entry)
+
+    # save the list of approach dictionaries in json format
+    with open(filename, "w") as outfile:
+        json.dump(result_dict, outfile, indent=2)
